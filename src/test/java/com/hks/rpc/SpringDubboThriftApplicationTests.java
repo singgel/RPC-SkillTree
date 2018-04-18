b7c062c5a65171045cba4709aa0a31226429b8a6
hekuangsheng <hekuangsheng@163.com>
2018-04-18 08:55:36 +0800

    Initial commit

---
+++ src/test/java/com/hks/rpc/SpringDubboThriftApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hks.rpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDubboThriftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
