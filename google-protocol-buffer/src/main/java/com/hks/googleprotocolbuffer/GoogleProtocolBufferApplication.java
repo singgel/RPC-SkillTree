45ce4b466389b451d9acc1486c007a9a98da2822
heks <heks@yiche.com>
2018-10-29 14:37:54 +0800

    序列化

---
+++ google-protocol-buffer/src/main/java/com/hks/googleprotocolbuffer/GoogleProtocolBufferApplication.java
@@ -0,0 +1,12 @@
+package com.hks.googleprotocolbuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleProtocolBufferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleProtocolBufferApplication.class, args);
+	}
+}
