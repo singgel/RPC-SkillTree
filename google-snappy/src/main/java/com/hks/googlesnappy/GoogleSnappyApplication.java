5570fd3a3e368e1ab825eeecdd9e51c7691954ea
heks <heks@yiche.com>
2018-10-29 14:57:19 +0800

    google-snappy

---
+++ google-snappy/src/main/java/com/hks/googlesnappy/GoogleSnappyApplication.java
@@ -0,0 +1,12 @@
+package com.hks.googlesnappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSnappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSnappyApplication.class, args);
+	}
+}
