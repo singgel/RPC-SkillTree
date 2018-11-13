23f7b4286b8777862e21e6cb69f44275075c89e5
heks <heks@yiche.com>
2018-11-13 13:52:32 +0800

    dubbo-api

---
+++ dubbo-api/src/main/java/com/hks/dubboapi/DubboApiApplication.java
@@ -0,0 +1,12 @@
+package com.hks.dubboapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboApiApplication.class, args);
+	}
+}
---
+++ dubbo-api/src/main/java/com/hks/dubboapi/service/GreetingService.java
@@ -0,0 +1,9 @@
+package com.hks.dubboapi.service;
+
+/**
+ * @Author: hekuangsheng
+ * @Date: 2018/11/13
+ */
+public interface GreetingService {
+    String sayHello(String name);
+}
