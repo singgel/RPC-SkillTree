7469045b831c70513805bfdae03b0f9d045996b4
heks <heks@yiche.com>
2018-11-14 14:54:52 +0800

    spring-dubbo-consumer init

---
+++ spring-dubbo-consumer/src/main/java/com/hks/springdubboconsumer/SpringDubboConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.hks.springdubboconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDubboConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDubboConsumerApplication.class, args);
+	}
+}
---
+++ spring-dubbo-consumer/src/test/java/com/hks/springdubboconsumer/SpringDubboConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hks.springdubboconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDubboConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
